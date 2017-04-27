aec7bb3539b1d8218942f3bc3af44080eee95340
Anna R <anna.rybkina@magine.com>
2017-04-27 13:29:06 +0200

    Aspectratio to custom player

--- externs/shaka/player.js
+++ externs/shaka/player.js
@@ -136,6 +136,8 @@ shakaExtern.Stats;
  *   frameRate: ?number,
  *   mimeType: ?string,
  *   codecs: ?string,
+ *   sar: ?number,
+ *   par: ?number,
  *   primary: boolean
  * }}
  *
@@ -172,6 +174,10 @@ shakaExtern.Stats;
  *   The MIME type of the content provided in the manifest.
  * @property {?string} codecs
  *   The audio/video codecs string provided in the manifest, if present.
+ * @property {?number} sar
+ *   The video aspect ratio provided in the manifest, if present.
+ * @property {?number} par
+ *   The video aspect ratio provided in the manifest, if present.
  * @property {boolean} primary
  *   True indicates that this in the primary language for the content.
  *   This flag is based on signals from the manifest.
